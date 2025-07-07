5c09d789b7523ea3c65741f84798b0599e5a4595
Zozowww <lorenzopriulla@gmail.com>
2025-07-07 03:49:36 +0200

    SideBarre+Bouton
    Plus besoin d'appeler HelloWorld on peut juste cliquer sur le bouton

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -2,26 +2,29 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('adj.helloWorld', () => {
-			const panel = vscode.window.createWebviewPanel(
-				'myWebview',
-				'My React Panel',
-				vscode.ViewColumn.One,
-				{
-					enableScripts: true,
-					localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'media')],
-				}
-			);
+    
+    // Commande Hello World qui ouvre le React 
+    const helloWorldCommand = vscode.commands.registerCommand('adj.helloWorld', () => {
+        const panel = vscode.window.createWebviewPanel(
+            'myWebview',
+            'My React Panel',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'media')],
+            }
+        );
 
-			const scriptPath = vscode.Uri.joinPath(context.extensionUri, 'media', 'index.html');
-			const html = fs.readFileSync(scriptPath.fsPath, 'utf8');
+        const scriptPath = vscode.Uri.joinPath(context.extensionUri, 'media', 'index.html');
+        const html = fs.readFileSync(scriptPath.fsPath, 'utf8');
 
-			panel.webview.html = html;
+        panel.webview.html = html;
 
-			panel.webview.onDidReceiveMessage(async msg => {
-				vscode.window.showInformationMessage(JSON.stringify(msg))
-			});
-		})
-	);
+        panel.webview.onDidReceiveMessage(async msg => {
+            vscode.window.showInformationMessage(JSON.stringify(msg));
+        });
+    });
+
+    // Ajout du disposable au contexte
+    context.subscriptions.push(helloWorldCommand);
 }
